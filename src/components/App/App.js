15f1b6fd8468a36fb9e6638bec3967320478dd09
unknown <dele17822@uvg.edu.gt>
2020-05-01 13:38:21 -0600

    Fixes

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+// Prueba
+
+function App() {
+  return (
+    <div className="App">
+      <p>HOLAAAAAAAAAAAA</p>
+    </div>
+  );
+}
+
+export default App;
